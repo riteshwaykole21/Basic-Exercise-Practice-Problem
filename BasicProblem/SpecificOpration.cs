0f3a577e0544fb0b7d68aba9765e5f48718363f8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-03-30 10:22:24 +0530

    [Ritesh]add : Special Opration

--- BasicProblem/SpecificOpration.cs
+++ BasicProblem/SpecificOpration.cs
@@ -14,6 +14,7 @@ namespace BasicProblem
             Console.WriteLine((35+5)%7);
             Console.WriteLine(14+-4*6/11);
             Console.WriteLine(2+15/6*1-7%2);
+            Console.WriteLine(2+5+7/12);
         }
     }
 }
