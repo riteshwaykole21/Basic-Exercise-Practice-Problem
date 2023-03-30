078b68ada173ef747b441ea82cc9ffd3543853de
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-03-30 09:30:26 +0530

    [Ritesh]add : Print Hello

---
+++ BasicProblem/Basics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Basics
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello ");
+            Console.WriteLine("My Name Is Ritesh ");
+        }
+    }
+}
